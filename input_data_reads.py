55eb9f87aafb6b7320c5b9185bf83bd11bd45ca4
rjx678 <43542361+rjx678@users.noreply.github.com>
2019-10-31 14:39:31 +0800

    Add files via upload

---
+++ input_data_reads.py
@@ -0,0 +1,157 @@
+# Copyright 2015 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""Functions for downloading and reading MNIST data."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+# import PIL.Image as Image
+import random
+import numpy as np
+import cv2
+import time
+
+#获得视频帧数据
+def get_frames_data(filename, num_frames_per_clip=6):
+    ''' Given a directory containing extracted frames, return a video clip of
+    (num_frames_per_clip) consecutive frames as a list of np arrays '''
+    # 返回的数组
+    ret_arr = []
+    for imagename in os.listdir(filename):
+        image_name = str(filename) + '/' + imagename
+        # 打开图像
+        img = cv2.imread(image_name)
+        # 将cv2的BGR转为RGB
+        img = img[:, :, ::-1]
+        # 获得图像数据
+        img_data = np.array(img, dtype=np.float32)
+        # print(img_data.shape)
+        # 将图像数据添加到ret_arr
+        ret_arr.append(img_data)
+    return ret_arr
+
+#读取视频帧及其标签
+def read_clip_and_label(filename, batch_size=8, num_frames_per_clip=6, crop_size=224, shuffle=True):
+
+    # 打开视频文件
+    lines = open(filename, 'r')
+    # 将文件lines转为list,每行为list类型lines的一个元素
+    lines = list(lines)
+    # 文件夹的名称先设置为空列表
+    read_dirnames = []
+    # data设置为空
+    data = []
+    # label设置为空列表
+    label = []
+    # 设置batch_index为0
+    batch_index = 0
+
+    # np.mean的shape=(16, 112, 112, 3)
+
+    np_mean = np.load('./crop_mean.npy')
+    # 如果start_pos没有指定，迫使shuffle = True
+    # Forcing shuffle, if start_pos is not specified
+    # if start_pos < 0:
+    #   shuffle = True
+    # 如果shuffle等于True
+    if shuffle:
+        # 获得视频的索引
+        video_indices = list(range(len(lines)))
+        # print('before',video_indices)
+        # 按照时间设置随机种子
+        random.seed(time.time())
+        # 将video_indices进行打乱
+        random.shuffle(video_indices)
+        # print('after',video_indices)
+    else:
+        # 顺序处理视频
+        # Process videos sequentially
+        # start_pos
+        # 从开始到len(lines)
+        video_indices = list(range(0, len(lines)))
+
+    # 变量视频的索引
+    for index in video_indices:
+        if (batch_index >= batch_size):
+            next_batch_start = index
+            break
+        # 获得第index个视频的名称和类别号
+        line = lines[index].strip('\n').split()
+        # 获得视频文件名称
+        dirname = line[0]
+        # 获得视频的类别号，表示属于什么类别
+        tmp_label = line[1]
+        # print(dirname,tmp_label)
+        # 得到视频帧的数据
+        tmp_data = get_frames_data(dirname, num_frames_per_clip)
+        # print(len(tmp_data))
+        # 图像数据img_datas初始化为空列表
+        img_datas = []
+        # 如果tmp_data的长度不等于0
+        if (len(tmp_data) != 0):
+            # 遍历tmp_data
+            for j in range(len(tmp_data)):
+                # 将tmp_data[j]转为np。uint8类型，再从数组转为Image类型
+                # img = Image.fromarray(tmp_data[j].astype(np.uint8))
+                img=tmp_data[j]
+                # 如果图像的宽大于图像的高,
+                if (img.shape[0] > img.shape[1]):
+                    # 比例
+                    scale = float(crop_size) / float(img.shape[1])
+                    img=cv2.resize(img, (int(img.shape[0] * scale + 1), crop_size))
+                else:
+                    # 缩放比例
+                    scale = float(crop_size) / float(img.shape[0])
+                    # 进行缩放，将短边缩放为crop_size，长边缩放为int(height/width*crop_size+1)
+                    img = cv2.resize(img, (crop_size, int(img.shape[1] * scale + 1)))
+
+                # 裁剪的宽的起始位置
+                crop_x = int((img.shape[0] - crop_size) / 2)
+                # 裁剪的高德起始位置
+                crop_y = int((img.shape[1] - crop_size) / 2)
+                # 获得中心裁剪后的图像，然后减去第j帧的平均值，shape=[224,224,3]
+                img = img[crop_x:crop_x + crop_size, crop_y:crop_y + crop_size, :]-np_mean[j]
+                # 将img添加到img_datas中
+                img_datas.append(img)
+            # img_datas=(num_frames_per_clip,crop_size,crop_size,3]的数据添加到data中
+
+            data.append(img_datas)
+
+            # 将视频的类别号添加到label中
+            label.append(int(tmp_label))
+            # batch_index累加
+            batch_index = batch_index + 1
+
+    # 将data转为numpy数组,类型转为np.float32,图像channles=3
+    # 这里np_arr_data的shape=[batch_size,num_frames_per_clip,crop_size,crop_size,channles]
+    np_arr_data = np.array(data, dtype=np.float32)
+    # np_arr_label的shape=(batch_size,)
+    np_arr_label = np.array(label, dtype=np.int32)
+
+    return np_arr_data, np_arr_label
+# rootPath = 'H:\\newdatasets190708-\\staticDataSet031\\SaveData'
+# os.chdir(rootPath)
+#
+# train_images, train_labels = read_clip_and_label(
+#         filename='./c3d_dataset/train.list',
+#         batch_size=6,
+#         num_frames_per_clip=6,
+#         crop_size=224,
+#         shuffle=True
+#     )
+#
+# print(train_images.shape,train_labels)
---
+++ run1.py
@@ -0,0 +1,114 @@
+from datetime import datetime
+import time
+from ResNet import ResNet_3D
+from utils1 import *
+import os
+from StaticModelCode import input_data_reads
+
+rootPath = r'H:\newdatasets190708-\sportsDataSet023\SaveData'
+os.chdir(rootPath)
+now = datetime.now()
+logs_path = "./graph50/" + now.strftime("%Y%m%d-%H%M%S")
+save_dir = "./checkpoints50/"
+
+image_size = 224
+num_classes = 3
+num_channels = 3
+num_epochs=10
+display=10
+num_steps = 5000
+batch_size = 6
+# Creating the ResNet model
+model = ResNet_3D(num_classes, image_size, num_channels)
+model.inference().pred_func().accuracy_func().loss_func().train_func()
+# Saving the best trained model (based on the validation accuracy)
+saver = tf.train.Saver()
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
+save_path = os.path.join(save_dir, 'best_validation')
+best_validation_accuracy = 0
+
+acc_b_all = acc_v_all=mean_v_acc=mean_acc = loss_v_all=mean_v_loss=loss_b_all = mean_loss = np.array([])
+sum_count = 0
+starttime=time.time()
+with tf.Session() as sess:
+
+    sess.run(tf.global_variables_initializer())
+    print("Initialized")
+    merged = tf.summary.merge_all()
+    batch_writer = tf.summary.FileWriter(logs_path + '/train/', sess.graph)
+    valid_writer = tf.summary.FileWriter(logs_path + '/test/',sess.graph)
+
+    for epoch in range(num_epochs):
+        epoch_start_time = time.time()
+        print('-----------------------------------------------------------------------------')
+        print('Epoch: {}'.format(epoch+1))
+
+        for step in range(num_steps):
+            train_images, train_labels = input_data_reads.read_clip_and_label(
+                filename='./c3d_dataset/train.list',
+                batch_size=6,
+                num_frames_per_clip=6,
+                crop_size=224,
+                shuffle=True
+            )
+
+            X_train, Y_train = train_images, train_labels
+            _ = sess.run(model.train_op,feed_dict={model.x:X_train,model.y:Y_train,model.keep_prob:0.5})
+
+
+            if step % display == 0:
+                feed_dict_batch = {model.x: X_train, model.y: Y_train, model.keep_prob: 1.0}
+                acc_b, loss_b = sess.run([model.accuracy, model.loss], feed_dict=feed_dict_batch)
+                acc_b_all = np.append(acc_b_all, acc_b)
+                loss_b_all = np.append(loss_b_all, loss_b)
+                mean_acc = np.mean(acc_b_all)
+                mean_loss = np.mean(loss_b_all)
+                print("Step {0}, training loss: {1:.5f}, training accuracy: {2:.05%}".format(step, mean_loss, mean_acc))
+
+                summary_tr = tf.Summary(value=[tf.Summary.Value(tag='Accuracy', simple_value=mean_acc)])
+                batch_writer.add_summary(summary_tr, sum_count * display)
+                summary_tr = tf.Summary(value=[tf.Summary.Value(tag='Loss', simple_value=mean_loss)])
+                batch_writer.add_summary(summary_tr, sum_count * display)
+                summary = sess.run(merged, feed_dict=feed_dict_batch)
+                batch_writer.add_summary(summary, sum_count * display)
+
+                val_images, val_labels = input_data_reads.read_clip_and_label(
+                    filename='./c3d_dataset/test.list',
+                    batch_size=6,
+                    num_frames_per_clip=6,
+                    crop_size=224,
+                    shuffle=True
+                )
+                feed_dict_val = {model.x: val_images, model.y: val_labels, model.keep_prob: 1.0}
+                acc_valid, loss_valid = sess.run([model.accuracy, model.loss], feed_dict=feed_dict_val)
+                acc_v_all = np.append(acc_v_all, acc_valid)
+                loss_v_all = np.append(loss_v_all,loss_valid)
+                # print(acc_v_all)
+                mean_v_acc=np.mean(acc_v_all)
+                mean_v_loss=np.mean(loss_v_all)
+                print("Step {0}, test mean acc: {1:.05%}".format(step,mean_v_acc))
+
+                summary_valid = tf.Summary(value=[tf.Summary.Value(tag='Accuracy', simple_value=mean_v_acc)])
+                valid_writer.add_summary(summary_valid, sum_count * display)
+                summary_v = sess.run(merged, feed_dict=feed_dict_val)
+                valid_writer.add_summary(summary_v, sum_count * display)
+                if mean_v_acc > best_validation_accuracy:
+                    # Update the best-known validation accuracy.
+                    best_validation_accuracy = mean_v_acc
+                    best_epoch = epoch
+                    # Save all variables of the TensorFlow graph to file.
+                    saver.save(sess=sess, save_path=save_path)
+                    # A string to be printed below, shows improvement found.
+                    improved_str = '*'
+                    print("Epoch {0}, test mean acc: {1:.05f}{2}"
+                          .format(epoch + 1, mean_v_acc, improved_str))
+                else:
+                    # An empty string to be printed below.
+                    # Shows that no improvement was found.
+                    improved_str = ''
+
+                sum_count += 1
+                # acc_b_all =loss_b_all=  mean_acc = mean_loss = y_pred_all = y_true_all = np.array([])
+
+print((time.time()-starttime)/3600)# 18 6.97hour
