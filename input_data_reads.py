062b3f0817bfc195a389b7d8137d12b819cd72b2
rjx678 <43542361+rjx678@users.noreply.github.com>
2019-10-31 14:41:13 +0800

    Update input_data_reads.py

--- input_data_reads.py
+++ input_data_reads.py
@@ -1,25 +1,4 @@
-# Copyright 2015 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-
-"""Functions for downloading and reading MNIST data."""
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import os
-# import PIL.Image as Image
 import random
 import numpy as np
 import cv2
@@ -143,15 +122,3 @@ def read_clip_and_label(filename, batch_size=8, num_frames_per_clip=6, crop_size
     np_arr_label = np.array(label, dtype=np.int32)
 
     return np_arr_data, np_arr_label
-# rootPath = 'H:\\newdatasets190708-\\staticDataSet031\\SaveData'
-# os.chdir(rootPath)
-#
-# train_images, train_labels = read_clip_and_label(
-#         filename='./c3d_dataset/train.list',
-#         batch_size=6,
-#         num_frames_per_clip=6,
-#         crop_size=224,
-#         shuffle=True
-#     )
-#
-# print(train_images.shape,train_labels)
